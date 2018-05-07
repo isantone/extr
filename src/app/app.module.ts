51cdb10513a7c7b457af1861a4ca2644876ca1bc
Anton Isakov <anton_isakov@epam.com>
2018-05-07 18:03:05 +0400

    Angular upgrade to the latest stable version [6.0.0]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './common/header/header.component';
+import { MenuBtnComponent } from './common/menu/menu-btn/menu-btn.component';
+import { MenuComponent } from './common/menu/menu.component';
+// import { ClipboardDirective } from './clipboard.directive';
+import { ClipDirective } from './common/clip-dir/clipboard.directive';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuBtnComponent,
+    MenuComponent,
+    // ClipboardDirective,
+    ClipDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
